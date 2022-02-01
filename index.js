772d308089b6bc45fbdebfff93a3225290960a4b
Brandon Dega <brandondega@gmail.com>
2022-02-01 09:46:41 -0600

    finished trivia game

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let userName = window.prompt('Please enter your name.')
 let userScore = 0
-for(i = 0; i < questions.length; i++) {
-    let question = questions[i]
-    let userAnswer = window.prompt(question.text)
-    if(userAnswer === question.correctAnswer) {
-        userScore = userScore + 10
+let playAgain = true
+
+function playGame(){
+    for(i = 0; i < questions.length; i++) {
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+        if(userAnswer === question.correctAnswer) {
+            userScore = userScore + 10
+        }
     }
+    window.alert('Your score is '+userScore)
 }
 
-window.alert('Your score is '+userScore)
+while(playAgain === true) {
+    playGame()
+    let userChoice = window.prompt('Would you like to play again?  Answer yes or no.')
+    if(userChoice === 'yes') {
+        playAgain === true
+    }else{
+        playAgain === false
+        window.alert('Thanks for playing!')
+    }
+}
