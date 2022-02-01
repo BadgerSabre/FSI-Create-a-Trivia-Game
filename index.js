5a9730aabbcfde746c019a0e4bb3b6fcf8ce6afd
Brandon Dega <brandondega@gmail.com>
2022-02-01 09:33:11 -0600

    finished?

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 let userName = window.prompt('Please enter your name.')
+let userScore = 0
 for(i = 0; i < questions.length; i++) {
-    console.log(questions[i])
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer) {
+        userScore = userScore + 10
+    }
 }
+
+window.alert('Your score is '+userScore)
